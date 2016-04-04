e26e115b9bab23d6b7ba5a808b44e16706e04b5f
Brian Hicks <brian@brianthicks.com>
2016-04-04 10:02:26 -0500

    source: get words in context

--- src/Source.elm
+++ src/Source.elm
@@ -66,7 +66,7 @@ view address model =
   Html.div
     []
     [ Html.h1 [] [ Html.text model.title ]
-    , Html.p [] [ Html.text model.text ]
+    , Html.p [] [ Html.text (model |> wordsInContext |> toString) ]
     ]
 
 
@@ -80,3 +80,28 @@ slug model =
     |> String.toLower
     |> Regex.replace Regex.All (Regex.regex "[^\\w\\d\\-\\s]") (\_ -> "")
     |> Regex.replace Regex.All (Regex.regex "\\s+") (\_ -> "-")
+
+
+wordsInContext : Model -> List ( String, String )
+wordsInContext model =
+  let
+    begins =
+      "[¡¿]?"
+
+    word =
+      "[a-zA-ZÀ-ÖØ-öø-ȳ]+"
+
+    words =
+      "(" ++ word ++ "\\s?)+"
+
+    ends =
+      "[\\.!\\?:;]"
+  in
+    model.text
+      |> Regex.find Regex.All (Regex.regex (begins ++ words ++ ends))
+      |> List.concatMap
+          (\sentence ->
+            sentence.match
+              |> Regex.find Regex.All (Regex.regex word)
+              |> List.map (\word -> ( word.match, sentence.match ))
+          )
